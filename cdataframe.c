829f37fd489fdb46e6de23386f79896911aa4932
l1eju <wengjulie@hotmail.com>
2024-05-15 12:12:07 +0200

    Avancement cdataframe, finalisation partie alimentation + avancement opérations usuelles

--- cdataframe.c
+++ cdataframe.c
@@ -7,82 +7,18 @@
 #include "cdataframe.h"
 
 COLUMN ** cdataframe_vide(int size){
-    COLUMN ** cdataframe = (COLUMN **) malloc(size*sizeof(COLUMN));
+        COLUMN ** cdataframe = (COLUMN **) malloc(size*sizeof(COLUMN));
     return cdataframe;
 }
 
-void remplissage_cdataframe(COLUMN ***cdf, int *size){
-    int choice, r, len;
-    ENUM_TYPE type;
-    char title[N];
+void remplissage_cdataframe(COLUMN ***cdf, ENUM_TYPE type, int size, char *title, void *value[], int nb_value){
     COLUMN *col;
-    do {
-        printf("0:UINT, 1:INT, 2:CHAR, 3:FLOAT, 4:DOUBLE, 5:STRING, 6:STRUCTURE\n"
-               "Choisissez le type de donnee de la colonne :");
-        r=scanf("%d", &choice);
-    } while (r!=1 || choice>6 || choice<0);
-
-    switch (choice) {
-        case 0:
-            type=UINT;
-            break;
-        case 1:
-            type=INT;
-            break;
-        case 2:
-            type=CHAR;
-            break;
-        case 3:
-            type=FLOAT;
-            break;
-        case 4:
-            type=DOUBLE;
-            break;
-        case 5:
-            type=STRING;
-            break;
-        case 6:
-            type=STRUCTURE;
-            break;
-    }
-    printf("Entrez le titre de la colonne: ");
-    scanf(" %s", title);
-
     col=create_column(type, title);
-    printf("Entrez le nombre de valeur a entrer dans la colonne :");
-    scanf("%d", &len);
 
-    for (int i=0; i<len; i++){
-
-        printf("Insérer la valeur [%d] : ", i);
-        void* val;
-        switch (col->column_type) {
-            case UINT: {
-                unsigned int entree;
-                scanf("%u", &entree);
-                val=&entree;
-                break;
-            }
-            case INT: {
-                int entree;
-                scanf("%d", &entree);
-                break;
-            }
-            case CHAR:{
-                char entree;
-                scanf(" %c", &entree);
-                break;
-            }
-
-        }
-        insert_value(col, val);
-        }
-
-
-    printf("ok");
-    **cdf= realloc(**cdf, (*size + 1) * sizeof(COLUMN));
-    *cdf[*size] = col;
-    (*size)++;
+    for (int i=0; i<nb_value; i++) {
+        insert_value(col, value[i]);
+    }
+    add_col_cdataframe(cdf, &size, col);
 }
 
 void afficher_tout_cdataframe(COLUMN **cdf, int size){
@@ -109,9 +45,27 @@ void afficher_p_ligne_cdataframe(COLUMN **cdf, int size, int tete, int queue){
     }
 }
 
-void afficher_p_colonne_cdataframe(COLUMN **cdf, int size, int tete, int queue){
+void afficher_p_colonne_cdataframe(COLUMN **cdf, int tete, int queue){
     for (int i=tete; i<=queue; i++){
         printf("%s\n", cdf[i]->title);
         print_col(cdf[i]);
     }
+}
+
+void add_col_cdataframe(COLUMN ***cdf, int *size, COLUMN *col){
+    **cdf= realloc(**cdf, (*size+1)*sizeof(COLUMN));
+    *cdf[(*size)++]=col;
+}
+
+void del_col_cdataframe(COLUMN ***cdf, int *size, int col_del){
+    delete_column(cdf[col_del]);
+    **cdf= realloc(**cdf, (*size-1)*sizeof(COLUMN));
+    (*size)--;
+}
+
+void rename_title_of_col_in_cdf(COLUMN ***cdf,int index_col, char* new_title){
+    (*cdf)[index_col]->title=new_title;
+}
+
+int val_existence_cdataframe(COLUMN **cdf, int size, char* value){
 }
--- cdataframe.h
+++ cdataframe.h
@@ -6,12 +6,19 @@
 #define CDATAFRAME_WENG_TADRIST_PMP_CDATAFRAME_H
 
 
-
+//1. Alimentation
 COLUMN ** cdataframe_vide(int size);
-void remplissage_cdataframe(COLUMN ***cdf, int *size);
+void remplissage_cdataframe(COLUMN ***cdf, ENUM_TYPE type, int size, char *title, void *value[], int nb_value);
+
+//2. Affichage
 void afficher_tout_cdataframe(COLUMN **cdf, int size);
 void afficher_p_ligne_cdataframe(COLUMN **cdf, int size, int tete, int queue);
-void afficher_p_colonne_cdataframe(COLUMN **cdf, int size, int tete, int queue);
+void afficher_p_colonne_cdataframe(COLUMN **cdf, int tete, int queue);
 
+//3. Opérations usuelles
+void add_col_cdataframe(COLUMN ***cdf, int *size, COLUMN *col);
+void del_col_cdataframe(COLUMN ***cdf, int *size, int col_del);
+void rename_title_of_col_in_cdf(COLUMN ***cdf,int index_col, char* new_title);
+int val_existence_cdataframe(COLUMN **cdf, int size, char* value);
 
 #endif //CDATAFRAME_WENG_TADRIST_PMP_CDATAFRAME_H
